37c0d06525455bd50b89563287eb60e9e7aa35c9
pdkhoa <padakait@gmail.com>
2022-05-09 21:39:51 +0700

    commit func TwoSum in LeetCode

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,22 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            int[] value = TwoSum(new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11);
+        }
+
+        private int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            for(int i=0; i < nums.Length; i++)
+            {
+                int needNum = target - nums[i];
+                if(dictionary.ContainsKey(needNum) && dictionary[needNum] != i)
+                {
+                    return new int[] { i, dictionary[needNum] };
+                }
+                dictionary[nums[i]] = i;
+            }
+            return new int[] { nums[0], nums[1] };
         }
     }
 }
